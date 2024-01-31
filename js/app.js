746c5b252051365c1e6971b0149e6ae9edd78664
lenovo <Mobinabakhshipour.1380@gmail.com>
2024-01-31 18:32:37 +0330

    save ToDo even by refreshing and closing the  browser

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+let loacalStorageTODO = [];
+let getLoacalStorageTODO = [];
+let LocalStorageArrNumber = JSON.parse(localStorage.getItem('saveToDo'));
+let i = LocalStorageArrNumber.length;
 let inputContent = document.getElementById('itemInput');
 let addBtnElem = document.getElementById('addButton');
 inputContent.addEventListener('keypress',  addNewTodo)
@@ -5,8 +9,10 @@ addBtnElem.addEventListener('click',  addNewTodo)
 
 function addNewTodo (event){
     if(event.key === "Enter" || event.type ==="click"){
-       console.log(inputContent.value);
-       let newToDoContent = inputContent.value
+        loacalStorageTODO[i] =inputContent.value;
+        i++;
+         localStorage.setItem('saveToDo' ,JSON.stringify(loacalStorageTODO));
+
 
        let newLi = document.createElement('li')
        newLi.classList.add('completed')
@@ -33,7 +39,40 @@ function addNewTodo (event){
        UlElem.append(newLi)
 
        inputContent.value= ""
-    //   newLi.classList.add("completed well")
     
+    }
+}
+
+window.onload = function (){
+    getLoacalStorageTODO = JSON.parse(localStorage.getItem('saveToDo'))
+    
+    
+    if( getLoacalStorageTODO.length != 0){
+       getLoacalStorageTODO.forEach( function (item) {
+
+       let newLi = document.createElement('li')
+       newLi.classList.add('completed')
+       newLi.classList.add('well')
+
+       let addToDoLable = document.createElement('label')
+       addToDoLable.innerHTML = item;
+       newLi.append(addToDoLable)
+
+       let addCompleteBtn = document.createElement('button')
+       addCompleteBtn.classList.add('btn-success')
+       addCompleteBtn.classList.add('btn')
+       addCompleteBtn.innerHTML = 'Complete'
+       newLi.append(addCompleteBtn)
+
+       let addDeleteBtn = document.createElement('button')
+       addDeleteBtn.classList.add('btn-danger')
+       addDeleteBtn.classList.add('btn')
+       addDeleteBtn.innerHTML = 'Delete'
+       newLi.append(addDeleteBtn)
+
+
+       let UlElem = document.getElementById('todoList')
+       UlElem.append(newLi)
+        });
     }
 }
